317b13b28eef9be0dfa035a53f0f6a53f98078c4
Jason Couture <jcouture@pssproducts.com>
2016-10-14 23:44:52 -0400

    Basic start for Swiss chat bot

---
+++ Swiss.Core/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Swiss.Core
+{
+    public interface IChatServerFactory
+    {
+        IChatServer CreateServerInstance(string key);
+    }
+
+    public enum ConnectionState
+    {
+        Disconnected,
+        Connecting,
+        Authenticating,
+        Connected,
+        Disconnecting
+    }
+
+    public interface IChatServer
+    {
+        string Name { get; set; }
+        ConnectionState ConnectionState { get; }
+        Task ConnectAsync(CancellationToken cancellationToken);
+        Task DisconnectAsync(CancellationToken cancellationToken);
+    }
+    public class DisposeCallback : IDisposable
+    {
+        public Action OnDispose { get; set; }
+        public void Dispose()
+        {
+            OnDispose();
+        }
+    }
+    public interface IChatChannel
+    {
+        Task SendMessageAsync(string message);
+    }
+
+    public interface IChatMessage
+    {
+        IChatChannel Channel { get; }
+        string Message { get; }
+        IChatUser User { get; }
+    }
+
+    public interface IChatUser
+    {
+        string Name { get; }
+    }
+
+    public class ChatMessage : IChatMessage
+    {
+        public virtual IChatChannel Channel { get; set; }
+        public virtual string Message { get; set; }
+        public virtual IChatUser User { get; set; }
+
+    }
+}
---
+++ Swiss.FakeServer/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Swiss.Core;
+
+namespace Swiss.FakeServer
+{
+    public class TestServerFactory : IChatServerFactory
+    {
+        private readonly IWindsorContainer _container;
+
+        public TestServerFactory(IWindsorContainer container)
+        {
+            _container = container;
+        }
+
+        private readonly ConcurrentDictionary<string, IChatServer> _chatServerInstances = new ConcurrentDictionary<string, IChatServer>(StringComparer.InvariantCultureIgnoreCase);
+
+        public IChatServer CreateServerInstance(string key)
+        {
+            return _chatServerInstances.GetOrAdd(key, GetChatServerInstanceFromDependencyFramework);
+        }
+
+        private IChatServer GetChatServerInstanceFromDependencyFramework(string arg)
+        {
+            return _container.Resolve<TestServer>(new { name = arg });
+        }
+    }
+
+    public class TestServerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<TestServer>().ImplementedBy<TestServer>().LifestyleTransient(),
+                Component.For<IChatServerFactory>().ImplementedBy<TestServerFactory>().LifestyleSingleton()
+                );
+        }
+    }
+
+    public class TestServer : IChatServer
+    {
+        public TestServer(string name)
+        {
+            Name = name;
+            ConnectionState = ConnectionState.Disconnected;
+        }
+        private bool _connected = false;
+        public string Name { get; set; }
+        public ConnectionState ConnectionState { get; set; }
+        public async Task ConnectAsync(CancellationToken cancellationToken)
+        {
+            if (ConnectionState != ConnectionState.Disconnected) throw new InvalidOperationException("Already connected");
+            ConnectionState = ConnectionState.Connecting;
+            await Task.Delay(1000, cancellationToken);
+            ConnectionState = ConnectionState.Authenticating;
+            await Task.Delay(500, cancellationToken);
+            ConnectionState = ConnectionState.Connected;
+        }
+
+        public async Task DisconnectAsync(CancellationToken cancellationToken)
+        {
+            if (ConnectionState == ConnectionState.Disconnected || ConnectionState == ConnectionState.Disconnecting)
+                return;
+            ConnectionState = ConnectionState.Disconnecting;
+            await Task.Delay(500, cancellationToken);
+            ConnectionState = ConnectionState.Disconnected;
+        }
+    }
+
+
+}
